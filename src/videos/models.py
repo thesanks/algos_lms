6f9082aeeafdac36bc38796d8339331fa9d051ec
Macbook <apple@Apples-MacBook-Pro.local>
2018-08-06 15:49:47 +0100

    made changes to Video model

--- src/videos/models.py
+++ src/videos/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
+    title      = models.CharField(max_length=120)
     embed_code = models.TextField()
+    timestamp  = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.embed_code
+        return self.title
