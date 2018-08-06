df793ceb25593bf302d2c019284462911be5bbd7
Macbook <apple@Apples-MacBook-Pro.local>
2018-08-06 18:05:31 +0100

    updated admin options

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Video
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_filter     = ['title', 'timestamp']
+    list_display    = ['title', 'timestamp', 'updated']
+    readonly_fields = ['updated', 'timestamp', 'short_title']
+    search_fields   = ['title', 'embed_code']
+
+
+    class Meta:
+        model = Video
+
+    def short_title(self, obj):
+        return obj.title[:3]
+
+
+admin.site.register(Video, VideoAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -5,6 +5,7 @@ class Video(models.Model):
     title      = models.CharField(max_length=120)
     embed_code = models.TextField()
     timestamp  = models.DateTimeField(auto_now_add=True)
+    updated    = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
